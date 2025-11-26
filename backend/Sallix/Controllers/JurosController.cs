8b007f21dab8f014c76895bdb031fd28ed67fe23
Ruthless-n <ruthcnovais@outlook.com>
2025-11-26 15:24:38 -0300

    feat: calculo de juros

---
+++ backend/Sallix/Controllers/JurosController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Sallix.Models;
+
+namespace Sallix.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class JurosController : ControllerBase
+    {
+        private readonly Services.JurosService _jurosService;
+
+        public JurosController(Services.JurosService jurosService)
+        {
+            _jurosService = jurosService;
+        }
+
+        [HttpPost("calcular-juros")]
+        public IActionResult CalcularJuros([FromQuery] decimal valorInicial, [FromQuery] DateTime dataVencimento)
+        {
+            if (valorInicial <= 0 || dataVencimento == default)
+            {
+                return BadRequest("Dados inválidos.");
+            }
+
+            var resultado = _jurosService.CalcularValorJuros(valorInicial, dataVencimento);
+            return Ok(resultado);
+        }
+    }
+}
---
+++ backend/Sallix/Services/JurosService.cs
@@ -0,0 +1,22 @@
+﻿namespace Sallix.Services
+{
+    public class JurosService
+    {
+        public decimal CalcularValorJuros(decimal valorInicial, DateTime dataVencimento)
+        {
+            DateTime dataHoje = DateTime.Now;
+
+            if (dataHoje <= dataVencimento)
+            {
+                return valorInicial;
+            }
+
+            int diasAtraso = (dataHoje - dataVencimento).Days;
+            decimal taxaJurosDiaria = 0.025m;
+            decimal jurosPercentTotal = diasAtraso * taxaJurosDiaria;
+            decimal valorComJuros = valorInicial + (valorInicial * jurosPercentTotal);
+
+            return Math.Round(valorComJuros, 2);
+        }
+    }
+}
